87c93309d3f1140283974cce42bcbffb214edf20
christian villaescusa <christian.villaescusa@gmail.com>
2017-05-08 18:03:09 +0200

    translate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'control/game'
+
+  get 'control/score'
+
+   get 'game', to: 'control#game'
+   get '/', to: 'control#game'
+   get 'score', to: 'control#score'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
